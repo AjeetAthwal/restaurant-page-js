fbfdb55b08b70998fa39bde30d97f82cd87a14e5
Ajeet <ajeet_athwal@hotmail.co.uk>
2020-12-01 12:08:57 +0000

    added fake contact info

--- src/contactLoad.js
+++ src/contactLoad.js
@@ -3,7 +3,17 @@ const contactLoad = (mainDiv, toggle, headers) => {
     h1Tag.innerText = "Contact Us"
     h1Tag.classList.add("subcontent");
 
+    const h2EmailTag = document.createElement("h2");
+    h2EmailTag.innerText = "Mobile: 012 - 345 - 678"
+    h2EmailTag.classList.add("subcontent");
+
+    const h2MobileTag = document.createElement("h2");
+    h2MobileTag.innerText = "Email:  restaurant@restaurant.com"
+    h2MobileTag.classList.add("subcontent");
+
     mainDiv.appendChild(h1Tag);
+    mainDiv.appendChild(h2EmailTag)
+    mainDiv.appendChild(h2MobileTag)
 
     toggle("contact", headers);
 }
